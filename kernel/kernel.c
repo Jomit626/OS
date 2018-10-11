d0ad7348daf9091c4838facf660aa469c70a6635
Jomit <576903846@qq.com>
2018-10-11 09:41:46 +0800

    cls termitor to white

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,11 +1,17 @@
-int func(void){
-    //func();
-    char *video_memory = (char *)0xb8000; //for test
-    *video_memory = 'D';
-}
+void print_block(int row,int col,char color);
 
 int main(void){
-    func();
-    char *video_memory = (char *)0xb8000; //for test
-    *video_memory = 'A';
+    int i = 0;
+    int j = 0;
+    print_block(10, 10,0xf0);
+    for (i = 0; i <= 25;i++){
+        for (j = 0; j <= 80;j++)
+            print_block(i, j,0xf0);
+    }
+}
+
+void print_block(int row,int col,char color) {
+    char *video_memory = (char *)(0xb8000+ 2*(80*row + col));
+    *video_memory = '\0';
+    *(video_memory-1) = color;
 }
